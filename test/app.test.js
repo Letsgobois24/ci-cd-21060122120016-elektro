6c262a500537c79be535294b57da54acbe44ee1c
Letsgobois24 <rayhanfariz@students.undip.ac.id>
2024-11-23 15:14:20 +0700

    Update debugging test for app.test.js

--- test/app.test.js
+++ test/app.test.js
@@ -11,18 +11,3 @@ const response = await request(app).get('/');
 expect(response.status).toBe(200);
 expect(response.text).toBe('Hello, GitHub Actions!\n');
 });
-
-test('GET / responds with 200 status', async () => {
-    const response = await request(app).get('/');
-    expect(response.status).toBe(200);
-});
-
-test('GET / responds with text content type', async () => {
-    const response = await request(app).get('/');
-    expect(response.headers['content-type']).toBe('text/plain');
-});
-
-test('GET / responds with non-empty body', async () => {
-    const response = await request(app).get('/');
-    expect(response.text).not.toBe('');
-});
